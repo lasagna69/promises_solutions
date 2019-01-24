690cecae55ce4fd5fca67b5bf2b607bca35158e6
Thomas Cruz <tom.ortega47@gmail.com>
2019-01-24 17:11:17 +0800

    first commit, solutions up to asynchronous

---
+++ asynchronous.js
@@ -0,0 +1,20 @@
+var promise = new Promise(function(resolve, reject){
+	resolve('PROMISE VALUE');
+}).then(console.log);
+
+console.log('MAIN PROGRAM');
+
+/********official solution
+var promise = new Promise(function (fulfill, reject) {
+  fulfill('PROMISE VALUE');
+});
+
+// At this point, the value of promise is already known.
+
+// If promise is not always asynchronous, console.log would be called with
+// 'PROMISE VALUE' here. This is not the case.
+
+promise.then(console.log);
+
+console.log('MAIN PROGRAM');
+**********/
